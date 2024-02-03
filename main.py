8e85afcc893d3a802715b1c77a29f293c34a39f3
RivarYoder01 <alyssa.stars.101@gmail.com>
2024-02-02 19:39:37 -0600

    Made opening docstring, meta data for one author (Myself), and wrote the tuplet for the planets

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+#!/user.bin.env python3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Usage: Planet_Distances.py
+User inputs two different planets to calculate the distance between them.
 
+GitHub: https://github.com/caleb-jenkinson15/Planet_Distances
+"""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# metadata (RY- Currently written as in set up video, will be altered to include second programmer)
+__author__ = 'Rivar Yoder'
+__date__ = '2023/07/02'
+__email__ = 'alyode01@wsc.edu'
+__version__ = '0.0.1'
 
+MAX_LINE = 60
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+PLANETS = (('Mercury', 57),
+           ('Venus', 108),
+           ('Earth', 150),
+           ('Mars', 228),
+           ('Jupiter', 779)
+           ('Saturn', 1430),
+           ('Uranus', 2880)
+           ('Neptune', 4500)
+)
